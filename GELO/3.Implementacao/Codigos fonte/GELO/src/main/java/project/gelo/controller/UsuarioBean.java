2f2a98334f28b3b7c790511c51a7aed6765ab505
User7558 <82788197+User7558@users.noreply.github.com>
2026-09-23 22:47:28 -0300

    Continuei a fazer o usuário

---
+++ GELO/3.Implementacao/Codigos fonte/GELO/src/main/java/project/gelo/controller/UsuarioBean.java
@@ -0,0 +1,12 @@
+package project.gelo.controller;
+import project.gelo.model.Usuario;
+
+/**
+ *  Faz validação de entrada:
+ * Verifica se os dados inseridos estão corretos com o tipo de entrada esperada
+ * Verifica se todos os campos obrigatórios foram preenchidos
+ * etc
+ */
+public class UsuarioBean {
+    
+}
---
+++ GELO/3.Implementacao/Codigos fonte/GELO/src/main/java/project/gelo/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package project.gelo.service;
+
+/**
+ *  Faz validação de regra do sistema:
+ * Não deixa cadastrar o mesmo email duas vezes
+ * Redireciona leitor e admin pra tela certa
+ * etc
+ */
+public class UsuarioService {
+    
+}
